e8f2ce05ae503e3a755205bcad3891035a00795b
Alireza hasanzade <130076941+Arh321@users.noreply.github.com>
2024-11-18 13:26:44 +0330

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { Providers } from "../redux/rovider/Providers";
+import { Providers } from "../redux/povider/Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
